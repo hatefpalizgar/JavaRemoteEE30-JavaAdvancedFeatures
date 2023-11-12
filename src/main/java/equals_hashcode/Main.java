6b695ae0744a77f68350f9b22697eb4679f5ae64
Hatef Palizgar <hatef.palizgar@gamesys.co.uk>
2023-11-12 07:40:46 +0000

    adds equals_hashcode package

---
+++ src/main/java/equals_hashcode/Main.java
@@ -0,0 +1,6 @@
+package equals_hashcode;
+
+public class Main {
+    // TODO: what is equals() and hashCode() methods in Object class (look at EqualsAndHashcode.md)? How Map uses them?
+    // TODO: What it means in JDK documentation that "hashCode() should be consistent with equals()"?
+}
